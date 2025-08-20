9afb7b8dc83bacaedad07d492cca999fa99849c4
pragunkakkar684 <kakkarpragun.work@gmail.com>
2025-08-20 20:02:39 +0530

    made basic REPL

---
+++ repl.c
@@ -0,0 +1,65 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+typedef struct
+{
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} inputbuffer;
+
+inputbuffer *new_input_buffer()
+{
+    inputbuffer *input_buffer = malloc(sizeof(inputbuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
+
+    return input_buffer;
+}
+
+void print_prompt() { printf("Sup boy>"); }
+
+void read_input(inputbuffer *input_buffer)
+{
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0)
+    {
+        printf("Error reading Input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // ignore traling newline
+    input_buffer->input_length = bytes_read - 1; // this excludes newline from getting stored
+    input_buffer->buffer[bytes_read - 1] = 0;    // replaces newline with \0
+}
+
+void close_input_buffer(inputbuffer *input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+int main(int argc, char *argv[])
+{
+    inputbuffer *input_buffer = new_input_buffer();
+    while (true)
+    {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
+}
