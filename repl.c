278951e5b8024fed2bb44eeac0a66da5e0589ddc
pragunkakkar684 <kakkarpragun.work@gmail.com>
2025-08-30 01:03:44 +0530

    added executeresult enum

--- repl.c
+++ repl.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <stdint.h>
 
 typedef struct
 {
@@ -20,7 +21,9 @@ typedef enum
 typedef enum
 {
     PREPARE_SUCCESS,
-    PREPARE_URECOGNISED_STATEMENT
+    PREPARE_URECOGNISED_STATEMENT,
+    PREPARE_SYNTAX_ERROR,
+    PREPARE_UNRECOGNIZED_STATEMENT
 } prepareresult;
 
 typedef enum
@@ -28,6 +31,12 @@ typedef enum
     STATEMENT_INSERT,
     STATEMENT_SELECT
 } statementtype;
+
+typedef enum
+{
+    EXECUTE_SUCCESS,
+    EXECUTE_TABLE_FULL
+} executeresult;
 
 typedef struct
 {
@@ -80,26 +89,31 @@ metacommandresult do_meta_command(inputbuffer *input_buffer)
     }
 }
 
-prepareresult prepare_statement(inputbuffer* input_buffer, statement* statement){
-    if(strncmp(input_buffer->buffer, "insert", 6)==0){
+prepareresult prepare_statement(inputbuffer *input_buffer, statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
         statement->type = STATEMENT_INSERT;
         return PREPARE_SUCCESS;
     }
-    if(strcmp(input_buffer->buffer, "select")==0){
-        statement->type=STATEMENT_SELECT;
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
     }
     return PREPARE_URECOGNISED_STATEMENT;
 }
 
-void execute_statement(statement* statement){
-    switch(statement->type){
-        case(STATEMENT_INSERT):
+void execute_statement(statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
         printf("This is where we do an insert\n");
         break;
-        case(STATEMENT_SELECT):
+    case (STATEMENT_SELECT):
         printf("This is where we do a select\n");
-        break;  
+        break;
     }
 }
 
@@ -111,25 +125,29 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if(input_buffer->buffer[0]=='.'){
-            switch(do_meta_command(input_buffer)){
-                case(META_COMMAND_SUCCESS): continue;
-                case(META_COMMAND_UNRECOGNIZED_COMMAND):
+        if (input_buffer->buffer[0] == '.')
+        {
+            switch (do_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
                 printf("Unrecognized command '%s' \n", input_buffer->buffer);
                 continue;
             }
         }
 
         statement statement;
-        switch(prepare_statement(input_buffer, &statement)){
-            case PREPARE_SUCCESS: break;
-            case PREPARE_URECOGNISED_STATEMENT:
+        switch (prepare_statement(input_buffer, &statement))
+        {
+        case PREPARE_SUCCESS:
+            break;
+        case PREPARE_URECOGNISED_STATEMENT:
             printf("Unrecognized statement at start of'%s'. \n", input_buffer->buffer);
             continue;
         }
 
         execute_statement(&statement);
         printf("Executed\n");
-
     }
 }
