ce1712e8b1188ad1ed51b08bc47835db4b8a1860
pragunkakkar684 <kakkarpragun.work@gmail.com>
2025-08-30 01:33:17 +0530

    hardcoded a table

--- repl.c
+++ repl.c
@@ -41,7 +41,81 @@ typedef enum
 typedef struct
 {
     statementtype type;
+    row row_to_insert; // to be used only by insert statement
 } statement;
+
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 225
+
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} row;
+
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute) // a macro
+const uint32_t ID_SIZE = size_of_attribute(row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET+ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET+USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE+USERNAME_SIZE+EMAIL_SIZE;
+const uint32_t PAGE_SIZE = 4096;
+
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE/ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE*TABLE_MAX_PAGES;
+
+typedef struct{
+    uint32_t num_rows;
+    void* pages[TABLE_MAX_PAGES];
+}table;
+
+void print_row(row* row){
+    printf("(%d, %s, %s)\n", row->id,row->username,row->email);
+}
+
+void serialize_row(row* source, void* destination){
+    memcpy(destination+ID_OFFSET, &(source->id), ID_SIZE);
+    memcpy(destination+USERNAME_OFFSET, &(source->username),USERNAME_SIZE);
+    memcpy(destination+EMAIL_OFFSET, &(source->email),EMAIL_SIZE);
+}
+
+void deserialize_row(void* source, row* destination){
+    memcpy(&(destination->id), source+ID_OFFSET, ID_SIZE);
+    memcpy(&(destination->username), source+USERNAME_OFFSET, USERNAME_SIZE);
+    memcpy(&(destination->email), source+EMAIL_OFFSET, EMAIL_SIZE);
+}
+
+void* row_slot(table* table, uint32_t row_num){
+    uint32_t page_num = row_num/ROWS_PER_PAGE;
+    void* page = table->pages[page_num];
+    if(page==NULL){
+        //memory should be allcoated when we try ti accesspage
+        page = table->pages[page_num] = malloc(PAGE_SIZE);
+    }
+    uint32_t row_offset = row_num %ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset*ROW_SIZE;
+    return page+byte_offset;
+}
+
+table* new_table(){
+    table* table_ptr = (table*)malloc(sizeof(table));
+    table_ptr->num_rows = 0;
+    for (uint32_t i = 0; i < TABLE_MAX_PAGES; i++) {
+        table_ptr->pages[i] = NULL;
+    }
+    return table_ptr;
+}
+
+void free_table(table* table){
+    for(int i=0;table->pages[i];i++){
+        free(table->pages[i]);
+    }
+    free(table);
+}
 
 inputbuffer *new_input_buffer()
 {
@@ -49,7 +123,6 @@ inputbuffer *new_input_buffer()
     input_buffer->buffer = NULL;
     input_buffer->buffer_length = 0;
     input_buffer->input_length = 0;
-
     return input_buffer;
 }
 
